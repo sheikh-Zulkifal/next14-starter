f2d061988aeec08aae80d6e46d4adfb1997000c7
sheikh-Zulkifal <sheikhzulkifal1@gmail.com>
2024-03-23 01:41:04 +0500

    Done with all folderstructure and make responsive navbar creating links of nav bar

---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,64 @@
+"use client";
+import NavLink from "./navLink/navLink";
+import styles from "./links.module.css";
+import { useState } from "react";
+
+const links = [
+  {
+    title: "Homepage",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+];
+
+const Links = () => {
+  const [open, setOpen] = useState(false);
+
+  // Temporary
+  const session = true;
+  const isAdmin = true;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}
+        {session ? (
+          <>
+            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "/login" }} />
+        )}
+      </div>
+      <button
+        className={styles.menueButton}
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        Menue
+      </button>
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <NavLink item={link} key={link.title} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Links;
